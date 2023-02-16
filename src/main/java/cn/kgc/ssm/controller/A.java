17547e6335790fdfc67445f1434f4064f3aa06b7
SouthWind <southWind@163.com>
2023-02-16 14:00:43 +0800

    commit second

---
+++ src/main/java/cn/kgc/ssm/controller/A.java
@@ -0,0 +1,11 @@
+package cn.kgc.ssm.controller;
+
+/**
+ * @Author:邓湘标
+ * @Package:cn.kgc.ssm.controller
+ * @DESCRIPTION:
+ * @DATE: 2023/2/16 14:00
+ * @Version:1.0
+ */
+public class A {
+}
---
+++ src/main/java/cn/kgc/ssm/service/D.java
@@ -0,0 +1,11 @@
+package cn.kgc.ssm.service;
+
+/**
+ * @Author:邓湘标
+ * @Package:cn.kgc.ssm.service
+ * @DESCRIPTION:
+ * @DATE: 2023/2/16 14:00
+ * @Version:1.0
+ */
+public interface D {
+}
