d5efe87800dcbb9c076e86a8358ab177ec6f0c4f
SouthWind <southWind@163.com>
2023-02-17 11:25:22 +0800

    commit first by dxb

---
+++ src/main/java/cn/kgc/ssm/pojo/BookInfo.java
@@ -0,0 +1,11 @@
+package cn.kgc.ssm.pojo;
+
+/**
+ * @Author:邓湘标
+ * @Package:cn.kgc.ssm.pojo
+ * @DESCRIPTION:
+ * @DATE: 2023/2/17 11:22
+ * @Version:1.0
+ */
+public class BookInfo {
+}
---
+++ src/main/java/cn/kgc/ssm/service/BookInfoService.java
@@ -0,0 +1,11 @@
+package cn.kgc.ssm.service;
+
+/**
+ * @Author:邓湘标
+ * @Package:cn.kgc.ssm.service
+ * @DESCRIPTION:
+ * @DATE: 2023/2/17 11:23
+ * @Version:1.0
+ */
+public interface BookInfoService {
+}
